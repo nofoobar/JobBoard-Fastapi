119ccd1d2b1d29c493389c9a67a64471116c80ab
Superuser <ilovedjango3000@gmail.com>
2021-03-16 23:44:37 +0530

    Add support for serving static files

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
 
@@ -7,15 +8,15 @@ def include_router(app):
 	app.include_router(general_pages_router)
 
 
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 def start_application():
 	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 	include_router(app)
-	return app 
-
+	configure_static(app)
+	return app
 
 app = start_application()
 
-
-# @app.get("/")
-# def hello_api():
-#     return {"msg":"Hello API"}
