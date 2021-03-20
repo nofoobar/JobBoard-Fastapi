4fe7bee12d2204ae24f4e4ed0f43a158d319ee5f
Superuser <ilovedjango3000@gmail.com>
2021-03-20 18:46:42 +0530

    Add support for serving static files

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 class Settings:
     PROJECT_NAME:str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
+
+    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
+    POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine
+from db.base import Base 
 
 
 def include_router(app):
@@ -12,10 +14,16 @@ def configure_static(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+def create_tables():
+	print("create_tables")
+	Base.metadata.create_all(bind=engine)
+
+
 def start_application():
 	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 	include_router(app)
 	configure_static(app)
+	create_tables()
 	return app
 
 app = start_application()
