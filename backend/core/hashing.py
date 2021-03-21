8cb2234cd9b3c7887c0b732a4e6cf2d77c67ef6f
Superuser <ilovedjango3000@gmail.com>
2021-03-21 18:25:10 +0530

    Use passlib and bcrypt to support password hasing

---
+++ backend/core/hashing.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class Hasher():
+    @staticmethod
+    def verify_password(plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password)
+
+    @staticmethod
+    def get_password_hash(password):
+        return pwd_context.hash(password)
