26a12c538525f48e617ae5cb18c8e6e55441e0d7
Superuser <ilovedjango3000@gmail.com>
2021-03-24 21:54:17 +0530

    create jwt token

--- backend/core/config.py
+++ backend/core/config.py
@@ -20,5 +20,9 @@ class Settings:
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30  # in mins
+
 
 settings = Settings()
---
+++ backend/core/security.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from datetime import timedelta
+from typing import Optional
+
+from core.config import settings
+from jose import jwt
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
