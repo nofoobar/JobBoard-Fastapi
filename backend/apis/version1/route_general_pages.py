d290adfddc7bf19b9410cc16f22919df3ea3a647
Sourabh <sourabh@testpress.in>
2021-03-29 12:49:19 +0530

    Job listview in homepage

--- backend/apis/version1/route_general_pages.py
+++ backend/apis/version1/route_general_pages.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
-from fastapi import Request
+from fastapi import Request,Depends
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from db.repository.jobs import list_jobs
+from db.session import get_db
+
 
 
 templates = Jinja2Templates(directory="templates")
@@ -8,8 +13,8 @@ general_pages_router = APIRouter()
 
 
 @general_pages_router.get("/")
-async def home(request: Request):
-    # print(dir(request))
+async def home(request: Request,db: Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
     return templates.TemplateResponse(
-        "general_pages/homepage.html", {"request": request}
+        "general_pages/homepage.html", {"request": request,"jobs":jobs}
     )
